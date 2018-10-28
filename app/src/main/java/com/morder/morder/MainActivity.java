c7e96777f3dfbb943f9603e3b574eed4a46259c3
CROmarcos <marpavlov@foi.hr>
2018-10-28 19:31:22 +0100

    Napravljen gumb za prelazak početne stranice na pregled trenutnog stanja stolova, potrebno srediti prikaz stolova

--- app/src/main/java/com/morder/morder/MainActivity.java
+++ app/src/main/java/com/morder/morder/MainActivity.java
@@ -1,14 +1,40 @@
 package com.morder.morder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static com.morder.morder.R.id.btnPopisStolova;
+import static com.morder.morder.R.id.textView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(btnPopisStolova);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                PrikaziPopisStolova();
+            }
+        });
+        /*
+        button.setX(260);
+        button.setY(500);
+        */
+    }
 
+    public void PrikaziPopisStolova(){
+        Intent intent = new Intent(this, PrikazStolova.class);
+        startActivity(intent);
     }
 }
