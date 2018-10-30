8649d2118e71663d9da61206ec3910ae19475d9e
Danijel Pintarić <danijel.pintaric8@gmail.com>
2018-10-30 21:30:56 +0100

    Merge conflicts

--- app/src/main/java/com/morder/morder/MainActivity.java
+++ app/src/main/java/com/morder/morder/MainActivity.java
@@ -1,16 +1,8 @@
 package com.morder.morder;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import static com.morder.morder.R.id.btnPopisStolova;
-import static com.morder.morder.R.id.textView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +13,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
