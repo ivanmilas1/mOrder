cd0da000de4c7412de6b9691ac6f25d79a924d96
nidzoo305 <nikolagluhak@hotmail.com>
2018-11-02 20:28:02 +0100

    Dodana API biblioteka za dizajn u modulu aplikacije. Dodan activity "Narucivanje" koji prilikom pokretanja pokreće layout "narucivanje.xml". Layout "narucivanje.xml" sadrži izbornik, NavigationView, čije su stavke  definirane u "drawer_menu.xml". Na activity "MainActivity" dodan je gumb, klikom na gumb pokreće se activity "Narucivanje".

--- app/src/main/java/com/morder/morder/MainActivity.java
+++ app/src/main/java/com/morder/morder/MainActivity.java
@@ -1,16 +1,30 @@
 package com.morder.morder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.narucivanje);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), Narucivanje.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/morder/morder/Narucivanje.java
@@ -0,0 +1,13 @@
+package com.morder.morder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Narucivanje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.narucivanje);
+    }
+}
