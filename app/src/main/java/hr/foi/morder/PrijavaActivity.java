1392a9928bbc8d0d5fbeba041e35de00fa43c891
Danijel Pintarić <danijel.pintaric8@gmail.com>
2018-11-05 00:24:18 +0100

    Putting initial design

--- app/src/main/java/hr/foi/morder/PrijavaActivity.java
+++ app/src/main/java/hr/foi/morder/PrijavaActivity.java
@@ -3,11 +3,11 @@ package hr.foi.morder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PrijavaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_prijava);
     }
 }
