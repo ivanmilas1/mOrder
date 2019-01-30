1926ad00f85e10bd5d31ae0fcad09d53289a7fc6
Danijel Pintarić <danijel.pintaric8@gmail.com>
2019-01-30 14:57:28 +0100

    Preparing to merge with Master

--- app/src/main/java/hr/foi/morder/DodavanjeRadnikaActivity.java
+++ app/src/main/java/hr/foi/morder/DodavanjeRadnikaActivity.java
@@ -72,12 +72,12 @@ public class DodavanjeRadnikaActivity extends AppCompatActivity {
         loadDjelatnici();
 
         Intent intent = getIntent();
-        ((TextView)findViewById(R.id.input_name)).setText(intent.getStringExtra("Ime"));
-        ((TextView)findViewById(R.id.input_email)).setText(intent.getStringExtra("Email"));
+        ((TextView) findViewById(R.id.input_name)).setText(intent.getStringExtra("Ime"));
+        ((TextView) findViewById(R.id.input_email)).setText(intent.getStringExtra("Email"));
         String a = intent.getStringExtra("Azuriranje");
-        if(TextUtils.isEmpty(a)){
+        if (TextUtils.isEmpty(a)) {
             button.setText("Kreiraj korisnika");
-         }
+        }
     }
     /**
      * The dodaj Djelatnika method for adding new worker. Data from text fields is updated to Firestore database.
@@ -97,17 +97,17 @@ public class DodavanjeRadnikaActivity extends AppCompatActivity {
         String tip = spinnerTip.getSelectedItem().toString();
         switch (tip) {
             case "Konobar":
-                tipKorisnika =1;
+                tipKorisnika = 1;
                 break;
             case "Voditelj":
-                tipKorisnika =2;
+                tipKorisnika = 2;
                 break;
             case "Administrator":
-                tipKorisnika =3;
+                tipKorisnika = 3;
                 break;
         }
         if (!TextUtils.isEmpty(naziv)) {
-            if (!TextUtils.isEmpty(mail)&& android.util.Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+            if (!TextUtils.isEmpty(mail) && android.util.Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
                 if (!TextUtils.isEmpty(password)) {
                     Map<String, Object> djelatnik = new Djelatnik(naziv, password, mail, tipKorisnika).toMap();
                     databaseDjelatnik.collection("Djelatnik")
@@ -129,8 +129,9 @@ public class DodavanjeRadnikaActivity extends AppCompatActivity {
             Toast.makeText(this, "Morate unijeti ime korisnika", Toast.LENGTH_LONG).show();
         }
     }
+
     /**
-     * The load Djelatnici gets data from Firestore database collection "Djelatnik" , stores data in List djelatnikList and calls method djelatnikRecyclerAdapter.
+     * The load Djelatnici gets data from Firestore database collection "Djelatnik", stores data in List djelatnikList and calls method djelatnikRecyclerAdapter.
      * Creating new adapter for recycler viwe
      * Setting recycler view layout manager from application context
      * Setting the adapter into recycler view
@@ -143,33 +144,33 @@ public class DodavanjeRadnikaActivity extends AppCompatActivity {
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             List<Djelatnik> djelatnikList = new ArrayList<>();
-                            for(DocumentSnapshot documentSnapshot: task.getResult()){
+                            for (DocumentSnapshot documentSnapshot : task.getResult()) {
                                 Djelatnik djelatnik = documentSnapshot.toObject(Djelatnik.class);
                                 djelatnik.getImePrezime();
                                 djelatnik.getEmail();
                                 djelatnik.getTipDjelatnikaId();
                                 djelatnikList.add(djelatnik);
                             }
-                            djelatnikRecyclerAdapter = new DjelatnikRecyclerAdapter (getApplicationContext() ,djelatnikList ,  databaseDjelatnik);
+                            djelatnikRecyclerAdapter = new DjelatnikRecyclerAdapter(getApplicationContext(), djelatnikList, databaseDjelatnik);
                             RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
                             recyclerView.setLayoutManager(layoutManager);
                             recyclerView.setAdapter(djelatnikRecyclerAdapter);
-                        }
-                        else{
+                        } else {
                             Log.d("Error", "Error getting data");
                         }
                     }
                 });
     }
 
-    /**
-     * Build recycler view for creating new Recycler view and setting the adapter for this recycler view.
-     * Find recycler view by id
-     * Set layout manager and set adapter for recycler view
-     * @author Ivan Milas
-     */
+
+        /**
+         * Build recycler view for creating new Recycler view and setting the adapter for this recycler view.
+         * Find recycler view by id
+         * Set layout manager and set adapter for recycler view
+         * @author Ivan Milas
+         */
     public void buildRecyclerView(){
         recyclerView = findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
---
+++ app/src/main/java/hr/foi/morder/ScannerActivity.java
@@ -0,0 +1,13 @@
+package hr.foi.morder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+}
