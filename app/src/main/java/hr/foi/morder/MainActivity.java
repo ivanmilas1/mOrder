58e13d5bc63080b2dff931eb6bc3946e135df077
nidzoo305 <nikolagluhak@hotmail.com>
2019-01-31 14:55:47 +0100

    Dodana aktivnost odabira korisnika (naruči dostavu, qr kod, generirani kod) pri čemu se prilikom odabira "generirani kod" ispiše kod zadnjeg računa koji je za dostavu, slično tome funkcionira i odabir "gr kod".

--- app/src/main/java/hr/foi/morder/MainActivity.java
+++ app/src/main/java/hr/foi/morder/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void homeDelivery(View view) {
-        Intent intent = new Intent(this, DostavaActivity.class);
+        Intent intent = new Intent(this, OdabirKorisnikaActivity.class);
         startActivity(intent);
     }
 }
